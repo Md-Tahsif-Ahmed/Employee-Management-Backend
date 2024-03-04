5e71095967fa1cde9f7b4c15ab8cbe225e0cadd6
tahsif <tahsif.cse@gmail.com>
2024-03-04 11:19:35 +0600

    delete employee

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/employee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await employeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
   } catch (error) {
     console.error("Error:", error);
   } finally {
